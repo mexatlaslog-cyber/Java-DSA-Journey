145c578da5dea1302d79508d82704b4ee60149b7
mexatlaslog-cyber <your-new-email@example.com>
2026-04-04 01:26:43 +0530

    Initial clean commit

---
+++ src/IntroJava.java
@@ -0,0 +1,11 @@
+/*Problem: Introduction to Java
+* Description: Print basic messages to understand Java syntax
+* Approach: Use print statement to display output
+* * Time Complexity: O(1) * Space Complexity: O(1)
+* * Difficulty: Easy */
+    public class IntroJava {
+    public static void main(String[] args) {
+        System.out.println("Introduction to Java!");
+        System.out.println("Hello World!");
+    }
+}
